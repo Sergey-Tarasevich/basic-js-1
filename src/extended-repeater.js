5ee6e303583e043f990fa69df045bdd1545d45ff
Liza <elizabethspivak99@gmail.com>
2020-02-19 14:32:01 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,14 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+    const { separator, additionSeparator, additionRepeatTimes, repeatTimes } = { separator: '+', additionSeparator: '|', additionRepeatTimes: 1, repeatTimes: 1}
+    const userOptions = {separator, additionSeparator, additionRepeatTimes, repeatTimes, ...options }
+    let suffix = '';
+    Object.keys(userOptions).sort().map(options => {
+      if(options == 'addition') {
+        suffix = Array.from({length: userOptions.additionRepeatTimes === undefined ? 1 : userOptions.additionRepeatTimes }, (n, k) => k).map(el => new String(userOptions.addition).toString()).join(userOptions.additionSeparator)
+      }
+    })
+    
+    str = Array.from({length: userOptions.repeatTimes === undefined ? 1 : userOptions.repeatTimes }, (n, k) => k).map(el => new String(str).toString()  + suffix).join(userOptions.separator)
   
+    return str
+};
